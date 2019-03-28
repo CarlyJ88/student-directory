db2a277652868a9f2abe46515eee887be5dc9b04
Carly <carly.jenkinson65@googlemail.com>
2019-03-28 21:22:47 +0000

    followed DRY principle by extracting the push to students array into options method to avoid repetition of messages to user written in the code and simplified the while loop in input_students method. Also changed it so that the user has the option to break the loop at the start instead of the end of the loop

--- directory.rb
+++ directory.rb
@@ -1,29 +1,24 @@
-def input_students
-  students = []
-  puts "Please enter the names of the students"
-  name = gets.chomp.capitalize
+students = []
+def input_students(students)
+  name = ''
+  cohort = ''
+  while true do
+    puts "Please enter the names of the students or hit enter to escape"
+    name = gets.chomp.capitalize
+    if name.empty?
+      break
+    end
+    puts "Please enter your cohort"
+    cohort = gets.chomp.capitalize.to_sym
+    cohort = :April if cohort.empty?
+    options(students, name, cohort)
+    end
+end
 
-  puts "Please enter your cohort"
-  puts "To finish, just hit return twice unless you made a typo
-  then type delete to start again"
-  cohort = gets.chomp.to_sym.capitalize
-
-  while !name.empty? && !cohort.empty? do
+def options(students, name, cohort)
     students << { name: name, cohort: cohort,
       hobbies: :'climbing, yoga, swimming, watching Netflix',
       country: :UK, height: :'158cm' }
-      puts "Please enter the names of the students"
-      name = gets.chomp.capitalize
-      puts "Please enter your cohort"
-      puts "To finish, just hit return twice"
-      cohort = gets.chomp.to_sym.capitalize
-      cohort = :April if cohort.empty?
-      if cohort == :April
-        break
-      end
-  end
-
-  students
 end
 
 def print_header
@@ -34,10 +29,9 @@ end
 def print(students)
   count = 0
   while count < students.length
-  # students.each_with_index do |student, index|
     if students[count][:name].chr == 'C' && students[count][:name].length < 12
       puts "#{count +1}: #{students[count][:name]}".center(40)
-      puts  "(#{students[count][:cohort]} cohort)".center(40)
+      puts "(#{students[count][:cohort]} cohort)".center(40)
       puts "(Hobbies: #{students[count][:hobbies]})".center(40)
       puts "(Country of birth: #{students[count][:country]})".center(40)
       puts "(height: #{students[count][:height]})".center(40)
@@ -50,7 +44,20 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students."
 end
 
-students = input_students
+input_students(students)
 print_header
 print(students)
 print_footer(students)
+
+# def delete_cohort(students)
+#   puts "Did you enter that correctly?"
+#   answer = gets.chomp
+#   if answer == 'no'
+#     students.delete(:cohort)
+#     puts "Please re-enter your cohort"
+#     puts "To finish, just hit return twice"
+#     cohort = gets.chomp.to_sym.capitalize
+#   elsif answer == 'yes'
+#     input_students
+#   end
+# end
